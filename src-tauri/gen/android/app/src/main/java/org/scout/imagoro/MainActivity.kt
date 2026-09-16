c6121c22d1175094fb906f90fbd8b26c0b1181b9
wendigoro <gorogi@tutamail.com>
2026-09-16 02:23:14 -0700

    feat: Android APK verified - Tauri v2 android project + release-unsigned universal apk
    pnpm tauri android init+build (JDK17, SDK cmdline-tools/NDK r27c). APK at
    gen/android/app/build/outputs/apk/universal/release/ (25.9MB, unsigned release; sign w/ keystore for store).

---
+++ src-tauri/gen/android/app/src/main/java/org/scout/imagoro/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.scout.imagoro
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
